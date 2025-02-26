05f4da94680c271b8498f5cd50e465052f98083f
rinku kumar <rinkukumar412616@gmail.com>
2025-02-26 11:41:02 +0530

    mail system updated

--- src/util/mail.ts
+++ src/util/mail.ts
@@ -10,7 +10,6 @@ const transporter = nodemailer.createTransport({
   },
 } as nodemailer.TransportOptions);
 
-// async..await is not allowed in global scope, must use a wrapperaddress
 async function mail({
   address,
   subject,
@@ -18,23 +17,22 @@ async function mail({
   html,
 }: {
   address: string;
-  type?: string;
-  _id?: string;
   subject: string;
   text: string;
   html: string;
 }) {
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: "Rinku Kumar 👻<rinkukumar412616@gmail.com>", // sender address
-    to: address, // list of receivers
-    subject: subject, // Subject line
-    text: text, // plain text body
-    html: html, // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+  try {
+    const info = await transporter.sendMail({
+      from: "Rinku Kumar 👻<rinkukumar412616@gmail.com>", // sender address
+      to: address, // list of receivers
+      subject: subject, // Subject line
+      text: text, // plain text body
+      html: html, // html body
+    });
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.error("Error sending mail:", error);
+  }
 }
 
 export const createMailSystem = async ({
@@ -46,23 +44,26 @@ export const createMailSystem = async ({
   type: string;
   _id: string;
 }) => {
-  const token = randomUUID();
-  const DOMAIN = process.env.DOMAIN || "http://localhost:3000";
-  const user = await User.findByIdAndUpdate(_id, {
-    [`${type}Token`]: token, // generate a random token
-    [`${type}TokenExpire`]: new Date(Date.now() + 10 * 60 * 1000), // 10 minutes
-  });
-  if (type === "verify") {
-    const html = `<a href="${DOMAIN}/verify?TOKEN=${token}">Click here to verify your email</a>`;
-    const text = `${user.username}, please click the link below to verify your email: ${DOMAIN}/verify?TOKEN=${token}`;
-    const subject = "Verify your email";
-    await mail({ address, type, _id, subject, text, html });
-  } else if (type === "reset") {
-    const html = `<a href="${DOMAIN}/reset?TOKEN=${token}">Click here to reset your password</a>`;
-    const text = `${user.username}, please click the link below to reset your password: ${DOMAIN}/reset?TOKEN=${token}`;
-    const subject = "Reset your password";
-    await mail({ address, type, _id, subject, text, html });
+  try {
+    const token = randomUUID();
+    const DOMAIN = process.env.DOMAIN || "http://localhost:3000";
+    const user = await User.findByIdAndUpdate(_id, {
+      [`${type}Token`]: token, // generate a random token
+      [`${type}TokenExpire`]: new Date(Date.now() + 10 * 60 * 1000), // 10 minutes
+    });
+
+    if (type === "verify") {
+      const html = `<a href="${DOMAIN}/verify?TOKEN=${token}">Click here to verify your email</a>`;
+      const text = `${user.username}, please click the link below to verify your email: ${DOMAIN}/verify?TOKEN=${token}`;
+      const subject = "Verify your email";
+      await mail({ address, subject, text, html });
+    } else if (type === "reset") {
+      const html = `<a href="${DOMAIN}/reset?TOKEN=${token}">Click here to reset your password</a>`;
+      const text = `${user.username}, please click the link below to reset your password: ${DOMAIN}/reset?TOKEN=${token}`;
+      const subject = "Reset your password";
+      await mail({ address, subject, text, html });
+    }
+  } catch (error) {
+    console.error("Error in createMailSystem:", error);
   }
 };
-
-// mail().catch(console.error);
