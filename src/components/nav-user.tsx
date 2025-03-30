7d5b88e7514efa24d1df5a02d7061d0b5729faac
rinku kumar <rinkukumar412616@gmail.com>
2025-03-30 18:49:01 +0530

    dashboard main route bug fixed

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -47,7 +47,9 @@ export function NavUser() {
             >
               <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={user.picture} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <AvatarFallback className="rounded-lg">
+                  {initials}
+                </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user.name}</span>
