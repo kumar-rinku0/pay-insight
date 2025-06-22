7fb5e3d9488b4683c4e8c0330179ac436ee13a39
rinku kumar <rinkukumar412616@gmail.com>
2025-06-22 07:16:53 +0530

    vite preview proxy added

--- front-end-ts/vite.config.ts
+++ front-end-ts/vite.config.ts
@@ -6,6 +6,15 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    proxy: {
+      "/api": {
+        target: "https://pay-insight-0.vercel.app",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
   server: {
     host: "0.0.0.0",
     proxy: {
