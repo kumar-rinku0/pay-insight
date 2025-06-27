5ad350055f49a35e4de419364dcc7b210383527c
rinku kumar <rinkukumar412616@gmail.com>
2025-06-27 18:40:18 +0530

    env status changed to production

--- back-end/src/service/phonepay.js
+++ back-end/src/service/phonepay.js
@@ -7,7 +7,7 @@ import { StandardCheckoutClient, Env } from "pg-sdk-node";
 const clientId = process.env.PHONEPAY_CLIENT_ID;
 const clientSecret = process.env.PHONEPAY_SECRET_KEY;
 const clientVersion = 1; //insert your client version here
-const env = Env.SANDBOX; //change to Env.PRODUCTION when you go live
+const env = Env.PRODUCTION; //change to Env.PRODUCTION when you go live
 
 export const client = StandardCheckoutClient.getInstance(
   clientId,
