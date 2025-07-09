74d7b16fa5df566a0a069ae7a48f82bc171e6d76
rinku kumar <rinkukumar412616@gmail.com>
2025-07-09 17:04:59 +0530

    create a branch you need to be pro user

--- back-end/src/routes/branch.js
+++ back-end/src/routes/branch.js
@@ -6,6 +6,7 @@ import {
   handleGetBranchInfo,
 } from "../controllers/branch.js";
 import { onlyAdminUser } from "../middlewares/auth.js";
+import { isProUser } from "../controllers/subscription.js";
 
 const route = Router();
 
@@ -13,7 +14,9 @@ route.route("/").get((req, res) => {
   return res.status(200).json({ msg: "ok" });
 });
 
-route.route("/create").post(onlyAdminUser, wrapAsync(handleCreateBranch));
+route
+  .route("/create")
+  .post(onlyAdminUser, wrapAsync(isProUser), wrapAsync(handleCreateBranch));
 route.route("/info").get(wrapAsync(handleGetBranchInfo));
 route.route("/company").get(wrapAsync(handleFetchBranches));
 
