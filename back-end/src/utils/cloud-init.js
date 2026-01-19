b9aac9fa014106eae1ddb32b9fd95ac5432ce4e3
kumar-rinku0 <rinkukumar412616@gmail.com>
2026-01-19 23:55:55 +0530

    cloud init updated

--- back-end/src/utils/cloud-init.js
+++ back-end/src/utils/cloud-init.js
@@ -37,7 +37,6 @@ configDotenv(); // Load from .env file
 //   return next();
 // };
 
-import fs from "fs";
 import { v2 as cloudinary } from "cloudinary";
 const CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 const API_KEY = process.env.CLOUDINARY_API_KEY;
@@ -47,6 +46,28 @@ cloudinary.config({
   api_key: API_KEY,
   api_secret: API_SECRET,
 });
+
+export const handleGetCloudinarySign = (req, res) => {
+  const timestamp = Math.round(Date.now() / 1000);
+
+  const paramsToSign = {
+    timestamp,
+    folder: "sign_uploads",
+  };
+
+  const signature = cloudinary.v2.utils.api_sign_request(
+    paramsToSign,
+    API_SECRET,
+  );
+
+  return res.json({
+    timestamp,
+    signature,
+    apiKey: API_KEY,
+    cloudName: CLOUD_NAME,
+  });
+};
+
 export const handleUploadImage = async (req, res, next) => {
   if (!req.file) {
     return res
